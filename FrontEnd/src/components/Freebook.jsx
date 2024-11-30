846ff3f94ee05194071b39c10010c73eeaf1f6c2
salmanmalnas <salmanmalnas64@gmail.com>
2024-12-01 01:20:26 +0530

    Update API URLs to use deployed backend on Render

--- FrontEnd/src/components/Freebook.jsx
+++ FrontEnd/src/components/Freebook.jsx
@@ -10,7 +10,7 @@ function Freebook() {
   useEffect(()=>{
     const getBook= async () => {
       try{
-        const res = await axios.get("http://localhost:4004/book");
+        const res = await axios.get("https://book-store-deploy-backend.onrender.com/book");
         setBook(res.data)
       } catch(error){
         console.log("Error from get : ", error.response || error.message || error)
