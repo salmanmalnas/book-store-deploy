d2c93b65ce5a54e11085d86c4cd48c0c5c5c7842
salmanmalnas <salmanmalnas64@gmail.com>
2024-12-09 19:06:09 +0530

    Update frontend configuration and dependencies for Tailwind CSS setup

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/main.jsx', './src/App.jsx'], // Include the specific files using Tailwind classes
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
